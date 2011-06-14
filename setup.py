675aedc9bc1906c7d13911d6fd07212db28a3ee6
Chris Streeter <chris@chrisstreeter.com>
2011-06-13 18:02:28 -0700

    An initial setup.py.

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+
+import stockpile
+
+class mytest(test):
+    def run(self, *args, **kwargs):
+        from runtests import runtests
+        runtests()
+        # Upgrade().run(dist=True)
+        # test.run(self, *args, **kwargs)
+
+setup(
+    name='django-stockpile',
+    version=stockpile.__version__,
+    author='Chris Streeter',
+    author_email='pypi@chrisstreeter.com',
+    url='http://github.com/streeter/django-ticketing',
+    description = 'Generate tickets efficiently in a database in Django',
+    long_description=open('README.markdown').read(),
+    packages=find_packages(),
+    license='BSD',
+    zip_safe=False,
+    install_requires=[
+    ],
+    test_suite = 'stockpile.tests',
+    include_package_data=True,
+    cmdclass={"test": mytest},
+    classifiers=[
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
